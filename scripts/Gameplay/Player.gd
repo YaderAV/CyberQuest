40c9b1045ddc2f3f6814af639e47a755de08c420
Yader <yaderv@uninorte.edu.co>
2025-11-22 20:59:25 -0500

    Respawn

--- scripts/Gameplay/Player.gd
+++ scripts/Gameplay/Player.gd
@@ -2,7 +2,7 @@
 extends CharacterBody2D
 class_name Player
 
-const SPEED = 400
+const SPEED = 350
 const JUMP_VELOCITY = -350.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
